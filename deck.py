be8b11e28c6ff9ff0bec0986fe85fe239931773e
jeremysteele <jeremysteelewriter@gmail.com>
2024-01-23 12:21:21 -0800

    1.0 creating classes

---
+++ deck.py
@@ -0,0 +1,8 @@
+suits = ['hearts', 'diamonds', 'clubs', 'spades']
+ranks = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+
+new_deck = []
+for suit in suits:
+    for rank in ranks:
+        new_deck.append({suit, rank})
+
---
+++ poker.py
@@ -0,0 +1,18 @@
+from deck import new_deck
+
+class Game_instance:
+    def __init__(self, num_players):
+        self.num_players = num_players
+        self.deck = new_deck
+        self.position = 0
+        self.cards_public
+        self.cards_private
+
+class Player_instance:
+    def __init__(self, name, money):
+        self.name = name
+        self.money = money
+        self.hand
+        self.public_hand
+
+        
