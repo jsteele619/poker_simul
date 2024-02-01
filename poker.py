a833bc40505eef592dfe51a3962163368b29dc86
jeremysteele <jeremysteelewriter@gmail.com>
2024-01-31 16:59:14 -0800

    4.0 new

--- poker.py
+++ poker.py
@@ -21,6 +21,7 @@ class Game_instance:
         self.big_blind = None                           # Player
         
         self.cards_public = []
+        self.player_names = ['Alpha', 'Bravo', 'Chad', 'David', 'Eddie', 'Fred', 'George', 'Harry', 'Ian']
         self.stage_public = ['Pre', 'Flop', 'Turn', 'River', 'Show']
         self.which_stage = 0
     
@@ -34,7 +35,7 @@ class Game_instance:
     def create_game(self):
         # Create a player instance for each
         for x in range(self.num_players):
-            player = Player_instance('Player ' + str(x), 200)
+            player = Player_instance(self.player_names[x], 200)
             self.player_instances.append(player)
 
     def play_game(self):
@@ -42,7 +43,7 @@ class Game_instance:
         
         self.create_game_order()
         self.post_blinds()
-        print("Dealing cards to players")
+        print("Dealing cards to players:\n ")
         self.deal_cards()
         self.game_action()
         self.stage()
@@ -58,13 +59,11 @@ class Game_instance:
         self.which_stage = 0
 
     def game_action(self):
-        
         while True:                                              # While the last acting player 
-            if self.priority.has_bet and (self.priority.bet_this_round == self.player_priority_action):
-                break
-
             if self.everyone_folded():
-                print("winner")
+                break
+            
+            if self.priority.has_bet and (self.priority.bet_this_round == self.player_priority_action):
                 break
 
             elif self.priority.fold:
@@ -73,7 +72,6 @@ class Game_instance:
 
             elif self.priority.all_in:
                 self.priority.has_bet = True
-                self.priority.bet_this_round = self.player_priority_action         # To break for loop, sloppy
                 self.priority = self.priority.next
                 continue
 
@@ -81,7 +79,7 @@ class Game_instance:
                 bet_time = self.priority.game_action_player(self.player_priority_action)
                 self.process_action(bet_time)
 
-                print("Player Priority:", self.player_priority.name, "Priority:", self.priority.name, "Priority Action:", self.player_priority_action)
+                print("Active Player:", self.priority.name, "Player with Priority:", self.player_priority.name, "Priority Action:", self.player_priority_action, "\n")
                 self.priority = self.priority.next
         
         self.which_stage += 1 
@@ -92,7 +90,7 @@ class Game_instance:
             self.set_pot(bet_time[1])
             self.player_priority_action = self.priority.bet_this_round               # Needs work to finalize raise from called position
             self.player_priority = self.priority
-            print(self.priority.name, "has raised", self.player_priority_action)
+            print(self.priority.name, "has raised to", self.player_priority_action)
 
         if bet_time[0] == "call":
             print(self.priority.name, "has called")
@@ -100,7 +98,6 @@ class Game_instance:
         
         if bet_time[0] == "check":
             print(self.priority.name, "has checked")
-            pass
 
         if bet_time[0] == "fold":
             print(self.priority.name, "has folded")
@@ -108,7 +105,7 @@ class Game_instance:
         if bet_time[0] == "all in":
             self.set_pot(bet_time[1])
             self.player_priority_action = self.priority.total_bet
-            self.priority.bet_this_round = self.player_priority_action             # Sloppy
+            self.priority.bet_this_round = self.player_priority_action              # Sloppy
             print(self.priority.name, "is all-in")
 
     def create_game_order(self):
@@ -134,7 +131,7 @@ class Game_instance:
         
         self.big_blind.place_bet(self.big_blind_amount)
         self.set_pot(self.big_blind_amount)
-        print("posted blinds: sb:", self.small_blind.bet_this_round, "bb:", self.big_blind.bet_this_round, self.get_pot())
+        #print("posted blinds: sb:", self.small_blind.bet_this_round, "bb:", self.big_blind.bet_this_round, self.get_pot())
 
     def deal_cards(self):
         for player in self.player_instances:
@@ -160,19 +157,19 @@ class Game_instance:
         rand_card2 = random.choice(self.update_deck)
         self.cards_public.append(rand_card2)
         self.update_deck.remove(rand_card2)
-        print("Flop", self.print_cards(rand_card), self.print_cards(rand_card1), self.print_cards(rand_card2))
+        print("\nFlop", self.print_cards(rand_card), self.print_cards(rand_card1), self.print_cards(rand_card2))
 
     def deal_turn(self):
         rand_card = random.choice(self.update_deck)
         self.cards_public.append(rand_card)
         self.update_deck.remove(rand_card)
-        print("Turn:", self.print_cards(rand_card))
+        print("\nTurn:", self.print_cards(rand_card))
 
     def deal_river(self):
         rand_card = random.choice(self.update_deck)
         self.cards_public.append(rand_card)
         self.update_deck.remove(rand_card)
-        print("River", self.print_cards(rand_card))
+        print("\nRiver:", self.print_cards(rand_card))
 
     def reset_priority(self):
         self.priority = self.head.next
@@ -237,6 +234,8 @@ class Game_instance:
         for x in self.player_instances:
             if x.fold == False:
                 print(x.name, x.total_bet)
+            else:
+                print("Folded:", x.name, x.total_bet) 
 
 class Player_instance:
     def __init__(self, name, money):
@@ -255,58 +254,68 @@ class Player_instance:
 
     def game_action_player(self, action):
         # Decide what to do, currently random game actions
-        # Action refers to self.player_priority_action
+        
         self.has_bet = True
-        if self.all_in:
+        if self.all_in:                                 # Shouldn't be necessary
+            self.bet_this_round = action                 # To break the for loop, sloppy
             return ["check", 0]
         
-        elif action == self.bet_this_round:
+        elif action == self.bet_this_round:             # If the action is equal to your action this round
             x = random.randint(2, 3)
             if x == 3:
                 return ["check", 0]
             if x == 2:
-                val = self.place_bet(10 + action)
-                return ["raise", 10 + val]
+                val = self.place_bet(20 + action)
+                return ["raise", val]
+            if x == 1 :                                  # Not Currently Accessible
+                return ['all in', 10]
         
-        elif action > self.bet_this_round:
-            x = random.randint(0, 3)
-            if x == 0:
+        elif action >= self.money:                      # If the raise is more than your current money
+            x = random.randint(0, 1)
+            if x == 0:                              
+                val = self.place_bet(self.money)        # Call and All In
+                return ['all in', val]                   
+            else:
+                self.fold = True
+                return ['fold', 0] 
+        
+        elif action > self.bet_this_round:              # If the raise is more than you have bet this round
+            x = random.randint(1, 3)
+            if x == 0:                                  # Still strange
                 self.all_in = True
                 val = self.place_bet(self.money)
                 return ["all in", val] 
             if x == 1: 
-                val = self.place_bet(10 + action)
+                val = self.place_bet(20 + action - self.bet_this_round)
                 return ["raise", val]
             if x == 2:
-                val = self.place_bet(action) 
+                val = self.place_bet(action - self.bet_this_round) 
                 return ["call", val]
             if x == 3:
                 self.fold = True 
                 return ["fold", 0]
         else:
+            print('weird')
             return["fold", 0]
         
     def place_bet(self, amount):                        # Place the bet with impact on player side, process bet on game state side
         # Reason to place bet
         if amount > self.money:
-            val = amount % self.money
-            self.money = 0
+            val = self.set_money(self.money)
             self.all_in = True
-            self.bet_this_round += val
-            self.total_bet += val
             return val
-        else:
-            self.set_money(amount)
-            self.bet_this_round += amount
-            self.total_bet += amount
-            return amount
+        val = self.set_money(amount)
+        return val
 
     def get_money(self):
         return self.money
 
     def set_money(self, amount):            # Needs work
         self.money -= amount   
+        self.bet_this_round += amount
+        self.total_bet += amount
+        return amount
 
-new_round = Game_instance(6, 2, 4)
+new_round = Game_instance(8, 2, 4)
 new_round.create_game()
 new_round.play_game()
